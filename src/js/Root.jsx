7721d1647c00f22c6dbf1b71f736266c427e5e08
otiai10 <otiai10@gmail.com>
2016-02-21 07:01:12 +0900

    Output Copy

--- src/js/Root.jsx
+++ src/js/Root.jsx
@@ -1,16 +1,105 @@
 import React, {Component} from 'react';
 import TabList from './TabList';
 
+import _ from 'underscore';
+
 class Root extends Component {
   constructor(props) {
     super(props);
-    this.state = {tabs: []}
+    this.state = {tabs: [], output: '', opacity: 0}
     chrome.tabs.query({}, (tabs) => {
       this.setState({tabs: tabs});
     });
   }
   render() {
-    return <TabList tabs={this.state.tabs} />;
+    return (
+      <div className="columns">
+        <div className="column is-8">
+          <div className="columns">
+            <div className="column is-12">
+              <button className="button" onClick={this.removeDup.bind(this)}>Remove Duplicated URL</button>
+            </div>
+          </div>
+          <TabList tabs={this.state.tabs} />
+        </div>
+        <div className="column is-4">
+          <div className="columns">
+            <div className="column is-12">
+              <button className="button" onClick={this.toMarkdown.bind(this)}>Get Markdown</button>
+              { (this.state.output == '') ? null : <button className="button" onClick={this.clipcopy.bind(this)}>
+                  <i className="fa fa-paperclip"></i>
+                </button> }
+            </div>
+          </div>
+          <div
+            ref="note"
+            className="notification is-primary"
+            style={{
+              opacity: this.state.opacity,
+              display: (this.state.copied) ? 'inherit' : 'none'
+            }}>
+            <button className="delete"></button>
+            Copied on clipboard!
+          </div>
+          <div className="columns">
+            <div className="column is-12">
+              <p className="control">
+                <textarea
+                  readOnly
+                  ref="output"
+                  onFocus={this.selectOutput.bind(this)}
+                  className="textarea"
+                  rows={this.state.output.split('\n').length}
+                  value={this.state.output}
+                  style={{
+                    height: this.state.output.split('\n').length*50+'px',
+                    width: '100%', overflow: 'hidden'
+                  }}></textarea>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  removeDup() {
+    let dict = {};
+    this.setState({tabs: _.filter(this.state.tabs, (tab) => {
+      if (!!dict[tab.url]) return false;
+      else dict[tab.url] = true; return true;
+    })});
+  }
+  toMarkdown() {
+    let rows = [];
+    _.map(this.state.tabs, (tab) => {
+      let row = '- <img src="'+tab.favIconUrl+'" width="16px" height="16px"> ['+tab.title+']('+tab.url+')';
+      rows.push(row);
+    });
+    this.setState({output: rows.join("\n")});
+  }
+  clipcopy() {
+    this.selectOutput();
+    document.execCommand('copy');
+    this.setState({copied: true});
+    let i = 0;
+    let id = setInterval(() => {
+      this.setState({opacity: i});
+      i += 0.01;
+      if (i > 1) clearInterval(id);
+    }, 8);
+    setTimeout(() => {
+      let idout = setInterval(() => {
+        this.setState({opacity: i});
+        i -= 0.01;
+        if (i < 0) {
+          clearInterval(idout);
+          this.setState({copied: false});
+        }
+      });
+    }, 2000);
+  }
+  selectOutput() {
+    this.refs.output.select();
   }
 }
 export default Root;
--- src/js/Tab.jsx
+++ src/js/Tab.jsx
@@ -5,12 +5,12 @@ class Tab extends Component {
     return (
       <tr className="columns">
         <td className="column is-1">
-          <img className="image is-24x24" src={this.props.tab.favIconUrl} />
+          <img className="image is-16x16" src={this.props.tab.favIconUrl} />
         </td>
-        <td className="column is-8">
+        <td className="column is-10">
           <a href={this.props.tab.url} target="_blank">{this.props.tab.title}</a>
         </td>
-        <td className="column is-3">
+        <td className="column is-1">
           <i className="fa fa-times"></i>
         </td>
       </tr>
