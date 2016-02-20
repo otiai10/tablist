efc453b209ac542c5ad219156df35edbb7cc664b
otiai10 <otiai10@gmail.com>
2016-02-21 04:21:51 +0900

    Use bulma.css

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
 
+import 'bulma/css/bulma.css';
+
 ReactDOM.render(<Root />, document.getElementById('main'));
--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ module.exports = {
   output: {filename:'dist/js/app.js'},
   module: {
     loaders: [
-      // {test: /\.jsx$/, loader: 'jsx-loader'},
-      {test: /\.jsx?$/, loader: 'babel-loader'}
+      {test: /\.jsx?$/, loader: 'babel-loader'},
+      {test: /\.css$/, loader: 'style-loader!css-loader'}
     ]
   },
   resolve: {
